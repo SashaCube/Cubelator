a58efcd8ae43b162321c8ef528632aaf9ca6e7be
Oleksandr Havryliuk <sashagavgavgav@gmail.com>
2018-11-27 00:28:14 +0200

    set logic for result fragment

--- app/src/main/java/com/cube/oleksandr/havryliuk/evaluate/MainActivity.java
+++ app/src/main/java/com/cube/oleksandr/havryliuk/evaluate/MainActivity.java
@@ -10,10 +10,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String expresion;
-
-    private TextView resultTextView;
     private TextView fieldTextView;
 
+    private TextView currentResultField;
+    private ResultFragment resultFragment;
     private Evaluate calc;
 
 
@@ -22,9 +22,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
         expresion = "";
         calc = new Evaluate();
+
+        // init result fragment
+        resultFragment = new ResultFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.result_fragment, resultFragment)
+                .commit();
+
+        initView();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentResultField = resultFragment.getMainResult();
+        resultFragment.mark(currentResultField);
+    }
+
+    public void initView() {
+        TextView btn0 = findViewById(R.id.tv_0);
+        btn0.setOnClickListener(this);
+        TextView btn1 = findViewById(R.id.tv_1);
+        btn1.setOnClickListener(this);
+        TextView btn2 = findViewById(R.id.tv_2);
+        btn2.setOnClickListener(this);
+        TextView btn3 = findViewById(R.id.tv_3);
+        btn3.setOnClickListener(this);
+        TextView btn4 = findViewById(R.id.tv_4);
+        btn4.setOnClickListener(this);
+        TextView btn5 = findViewById(R.id.tv_5);
+        btn5.setOnClickListener(this);
+        TextView btn6 = findViewById(R.id.tv_6);
+        btn6.setOnClickListener(this);
+        TextView btn7 = findViewById(R.id.tv_7);
+        btn7.setOnClickListener(this);
+        TextView btn8 = findViewById(R.id.tv_8);
+        btn8.setOnClickListener(this);
+        TextView btn9 = findViewById(R.id.tv_9);
+        btn9.setOnClickListener(this);
+        ImageView btnAdd = findViewById(R.id.tv_add);
+        btnAdd.setOnClickListener(this);
+        ImageView btnSub = findViewById(R.id.tv_sub);
+        btnSub.setOnClickListener(this);
+        ImageView btnMul = findViewById(R.id.tv_mul);
+        btnMul.setOnClickListener(this);
+        ImageView btnDiv = findViewById(R.id.tv_div);
+        btnDiv.setOnClickListener(this);
+        TextView btnProcent = findViewById(R.id.tv_percent);
+        btnProcent.setOnClickListener(this);
+        TextView btnCancel = findViewById(R.id.tv_cancel);
+        btnCancel.setOnClickListener(this);
+        ImageView btnBack = findViewById(R.id.tv_back);
+        btnBack.setOnClickListener(this);
+        TextView btnResult = findViewById(R.id.tv_result_b);
+        btnResult.setOnClickListener(this);
+        TextView btnDot = findViewById(R.id.tv_dot);
+        btnDot.setOnClickListener(this);
+        ImageView btnSplit = findViewById(R.id.split_icon);
+        btnSplit.setOnClickListener(this);
+
+        fieldTextView = findViewById(R.id.tv_field);
+    }
+
+    public void putResult(){
+        currentResultField.setText(String.valueOf(calc.calculate(expresion)));
     }
 
     @Override
@@ -75,14 +138,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case (R.id.tv_mul):
                 expresion += " * ";
                 break;
+            case (R.id.tv_percent):
+                //TODO: percent from (double) expresion
+                break;
+            case (R.id.split_icon):
+                resultFragment.split(currentResultField);
+                break;
             case (R.id.tv_cancel):
                 expresion = "";
                 break;
             case (R.id.tv_back):
-                expresion = expresion.substring(0, expresion.length() - 3);
+                expresion = expresion.substring(0, expresion.length() - 1);
+                break;
+            case (R.id.tv_main_result):
+                resultFragment.mark(resultFragment.getMainResult());
+                break;
+            case (R.id.tv_result_1):
+                resultFragment.mark(resultFragment.getFirstResult());
+                break;
+            case (R.id.tv_result_2):
+                resultFragment.mark(resultFragment.getSecondResult());
                 break;
             case (R.id.tv_result_b):
-                resultTextView.setText(String.valueOf(calc.calculate(expresion)));
+                putResult();
                 expresion = "";
                 break;
             default:
@@ -90,49 +168,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         fieldTextView.setText(expresion);
     }
-
-    public void initView() {
-        TextView btn0 = findViewById(R.id.tv_0);
-        btn0.setOnClickListener(this);
-        TextView btn1 = findViewById(R.id.tv_1);
-        btn1.setOnClickListener(this);
-        TextView btn2 = findViewById(R.id.tv_2);
-        btn2.setOnClickListener(this);
-        TextView btn3 = findViewById(R.id.tv_3);
-        btn3.setOnClickListener(this);
-        TextView btn4 = findViewById(R.id.tv_4);
-        btn4.setOnClickListener(this);
-        TextView btn5 = findViewById(R.id.tv_5);
-        btn5.setOnClickListener(this);
-        TextView btn6 = findViewById(R.id.tv_6);
-        btn6.setOnClickListener(this);
-        TextView btn7 = findViewById(R.id.tv_7);
-        btn7.setOnClickListener(this);
-        TextView btn8 = findViewById(R.id.tv_8);
-        btn8.setOnClickListener(this);
-        TextView btn9 = findViewById(R.id.tv_9);
-        btn9.setOnClickListener(this);
-        ImageView btnAdd = findViewById(R.id.tv_add);
-        btnAdd.setOnClickListener(this);
-        ImageView btnSub = findViewById(R.id.tv_sub);
-        btnSub.setOnClickListener(this);
-        ImageView btnMul = findViewById(R.id.tv_mul);
-        btnMul.setOnClickListener(this);
-        ImageView btnDiv = findViewById(R.id.tv_div);
-        btnDiv.setOnClickListener(this);
-        TextView btnProcent = findViewById(R.id.tv_percent);
-        btnProcent.setOnClickListener(this);
-        TextView btnCancel = findViewById(R.id.tv_cancel);
-        btnCancel.setOnClickListener(this);
-        ImageView btnBack = findViewById(R.id.tv_back);
-        btnBack.setOnClickListener(this);
-        TextView btnResult = findViewById(R.id.tv_result_b);
-        btnResult.setOnClickListener(this);
-        TextView btnDot = findViewById(R.id.tv_dot);
-        btnDot.setOnClickListener(this);
-
-        resultTextView = findViewById(R.id.tv_result);
-        fieldTextView = findViewById(R.id.tv_field);
+    public void setCurrentResultField(TextView currentResultField){
+       this.currentResultField = currentResultField;
     }
 
     @Override
